c34f7841dbe3f97308631245441d80311a676120
himasnshusankhala04 <himanshu.sankhala04@gmail.com>
2021-03-13 23:54:31 +0530

    minor

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from mainApp import views
 
+from django.views.generic import RedirectView
 from django.views.static import serve
 from django.conf.urls import url
 from spaceExp import settings
@@ -11,4 +12,5 @@ urlpatterns = [
     
 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    url(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico'))
 ]
